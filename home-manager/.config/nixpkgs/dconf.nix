75dc56ae038d6d68c9442115089810c6fe5f99fc
Conor Fleming <conorfleming@outlook.com>
2021-02-12 21:04:46 -0600

    init

---
+++ home-manager/.config/nixpkgs/dconf.nix
@@ -0,0 +1,84 @@
+# Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
+{ lib, ... }:
+
+let
+  mkTuple = lib.hm.gvariant.mkTuple;
+in
+{
+  dconf.settings = {
+    "org/gnome/control-center" = {
+      "last-panel" = "sound";
+    };
+
+    "org/gnome/desktop/input-sources" = {
+      "current" = "uint32 0";
+      "sources" = [ (mkTuple [ "xkb" "us" ]) ];
+      "xkb-options" = [ "eurosign:e" ];
+    };
+
+    "org/gnome/desktop/interface" = {
+      "gtk-im-module" = "gtk-im-context-simple";
+      "text-scaling-factor" = 1.4;
+    };
+
+    "org/gnome/desktop/privacy" = {
+      "report-technical-problems" = true;
+    };
+
+    "org/gnome/evolution-data-server" = {
+      "migrated" = true;
+      "network-monitor-gio-name" = "";
+    };
+
+    "org/gnome/mutter" = {
+      "attach-modal-dialogs" = true;
+      "dynamic-workspaces" = true;
+      "edge-tiling" = true;
+      "focus-change-on-pointer-rest" = true;
+      "workspaces-only-on-primary" = false;
+    };
+
+    "org/gnome/settings-daemon/plugins/color" = {
+      "night-light-last-coordinates" = mkTuple [ 30.218499 (-97.7972) ];
+    };
+
+    "org/gnome/settings-daemon/plugins/xsettings" = {
+      "antialiasing" = "grayscale";
+      "hinting" = "slight";
+    };
+
+    "org/gnome/shell" = {
+      "enabled-extensions" = "@as []";
+    };
+
+    "org/gnome/shell/world-clocks" = {
+      "locations" = "@av []";
+    };
+
+    "org/gnome/system/location" = {
+      "enabled" = true;
+    };
+
+    "org/gnome/terminal/legacy" = {
+      "theme-variant" = "dark";
+    };
+
+    "org/gnome/terminal/legacy/profiles:" = {
+      "default" = "ed87f63d-9d01-4a71-8c5b-6f0d674b7b49";
+      "list" = [ "ed87f63d-9d01-4a71-8c5b-6f0d674b7b49" ];
+    };
+
+    "org/gnome/terminal/legacy/profiles:/:ed87f63d-9d01-4a71-8c5b-6f0d674b7b49" = {
+      "background-color" = "#282A36";
+      "bold-color" = "#6E46A4";
+      "bold-color-same-as-fg" = false;
+      "font" = "FiraCode Nerd Font Mono 12";
+      "foreground-color" = "#F8F8F2";
+      "palette" = [ "#262626" "#E356A7" "#42E66C" "#E4F34A" "#9B6BDF" "#E64747" "#75D7EC" "#EFA554" "#7A7A7A" "#FF79C6" "#50FA7B" "#F1FA8C" "#BD93F9" "#FF5555" "#8BE9FD" "#FFB86C" ];
+      "use-system-font" = false;
+      "use-theme-colors" = false;
+      "visible-name" = "Default";
+    };
+
+  };
+}
---
+++ home-manager/.config/nixpkgs/home.nix
@@ -0,0 +1,129 @@
+{ config, pkgs, ... }:
+let
+  nur-no-pkgs = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {};
+in
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  imports = [
+    nur-no-pkgs.repos.rycee.hmModules.emacs-init
+    ~/.config/nixpkgs/dconf.nix
+  ];
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "conor";
+  home.homeDirectory = "/home/conor";
+
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    gnome3.gnome-tweak-tool
+    dconf2nix
+    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+  ];
+
+  # Bash Config
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      emacs = "emacs -nw";
+    };
+    bashrcExtra = ''
+       eval `dircolors ~/.dir_colors/`
+    '';
+  };
+
+  # Git Config
+  programs.git = {
+    enable = true;
+    userName = "Conor Fleming";
+    userEmail = "conorfleming@outlook.com";
+    extraConfig = {
+      core.editor = "emacs";
+      github.username = "conorflemingATX";
+    };
+  };
+
+  # Ssh Config
+  programs.ssh = {
+    enable = true;
+  };
+
+  programs.gnome-terminal.profile.Default = {
+    font = "Fira Code";
+  };
+
+  # Emacs Config
+  programs.emacs = {
+    enable = true;
+  };
+
+  programs.emacs.init = {
+    enable = true;
+    recommendedGcSettings = true;
+    prelude = ''
+      ;; Disable Startup message
+      (setq inhibit-startup-message t)
+
+      ;; Clean up some visual cruft
+      (scroll-bar-mode -1)
+      (tool-bar-mode -1)
+      (tooltip-mode -1)
+      (set-fringe-mode 10)
+      (menu-bar-mode 10)
+      (column-number-mode)
+      (global-display-line-numbers-mode)
+    '';
+    usePackage = {
+      no-littering = {
+        enable = true;
+	config = ''
+	  (setq auto-save-file-name-transforms
+	    `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+	'';
+      };
+      dracula-theme = {
+        enable = true;
+	config = ''
+	  (load-theme 'dracula t)
+	'';
+      };
+      ivy = {
+        enable = true;
+	bind = { "C-s" = "swiper"; };
+	bindLocal = {
+          ivy-minibuffer-map = {
+	    "TAB" = "ivy-alt-done";
+	    "C-n" = "ivy-next-line";
+	    "C-p" = "ivy-previous-line";
+	  };
+	  ivy-switch-buffer-map = {
+	    "C-n" = "ivy-next-line";
+	    "C-p" = "ivy-previous-line";	    
+	  };
+        };
+	config = ''
+	  (ivy-mode 1)
+	'';
+      };
+      nix-mode = {
+        enable = true;
+      };
+      org = {
+        enable = true;
+	package = epkgs: epkgs.org-plus-contrib;
+      };
+    };
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.03";
+}
